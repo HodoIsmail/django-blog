edf42542bcf2bdab436888561f4ed7ae93987eb6
Hodo Ismail <ic17oq5i@students.codeinstitute.net>
2024-11-13 19:27:49 +0000

    amend urls

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import index
+from blog import views as blog_views
 
 urlpatterns = [
-    path('blog/', index , name='index'),
+    path('blog/', blog_views.index , name='index'),
     path('admin/', admin.site.urls),
 ]
